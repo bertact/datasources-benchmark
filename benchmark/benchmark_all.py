66e1c8801f6f916e3fba3beebbd14c4814eb3c7e
Berta Cabanas <berta.cabanas@basetis.com>
2025-06-01 08:18:19 +0200

    mongodb connection working

--- benchmark/benchmark_all.py
+++ benchmark/benchmark_all.py
@@ -1,13 +1,39 @@
 import psycopg2
+from pymongo import MongoClient
 
 try:
-    conn = psycopg2.connect(
+    conn_postgres = psycopg2.connect(
         dbname="benchmark_postgres",
         user="postgres",
         host="postgresql-db",
         password="password",
         port=5432,
     )
-    print("Connected to the DB")
+    print("Connected to Postgres DB")
 except:
-    print("Can't connect to the DB")
+    print("Can't connect to Postgres DB")
+
+
+cursor = conn_postgres.cursor()
+
+# try:
+#     cursor.execute(
+#         """CREATE TABLE PROJECTS (
+#             prj_id INTEGER,
+#             "name" VARCHAR(45) NOT NULL,
+#             budget INTEGER,
+#             "end" DATE,
+#             PRIMARY KEY(prj_id))"""
+#     )
+#     conn.commit()  # To commit changes to the disk
+
+# except Exception as e:
+#     cursor.execute("ROLLBACK")  # Rollback when fail
+#     print(e)
+
+
+try:
+    conn_mongo = MongoClient("mongo", 27017, username="admin", password="password")
+    print("Connected to Mongo DB")
+except:
+    print("Can't connect to Mongo DB")
