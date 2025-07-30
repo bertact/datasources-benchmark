1e8d82de60d7a53bec0d9400905f7b497a00cbc1
Berta Cabanas <berta.cabanas@basetis.com>
2025-07-30 15:43:32 +0200

    fix

--- benchmark/postgresql/postgres_operations.py
+++ benchmark/postgresql/postgres_operations.py
@@ -302,7 +302,7 @@ def execute_op_postgres(container_name):
     def one(row):
         return insert(row, table_name="employees")
 
-    run_parallel(one, work, Path(result_file),  max_workers=4)
+    run_parallel(one, work, Path(result_file), max_workers=4)
 
     restart_postgres(container_name)
 
@@ -318,7 +318,7 @@ def execute_op_postgres(container_name):
             id=id,
         )
 
-    run_parallel(one, select_ids, Path(result_file),  max_workers=4)
+    run_parallel(one, select_ids, Path(result_file), max_workers=4)
 
     restart_postgres(container_name)
 
@@ -334,7 +334,7 @@ def execute_op_postgres(container_name):
             city=city,
         )
 
-    run_parallel(one, list_cities, Path(result_file),  max_workers=4)
+    run_parallel(one, list_cities, Path(result_file), max_workers=4)
 
     restart_postgres(container_name)
 
@@ -347,7 +347,7 @@ def execute_op_postgres(container_name):
             id=id,
         )
 
-    run_parallel(one, update_ids, Path(result_file),  max_workers=4)
+    run_parallel(one, update_ids, Path(result_file), max_workers=4)
 
     restart_postgres(container_name)
 
@@ -388,4 +388,4 @@ def execute_op_postgres(container_name):
             id=id,
         )
 
-    run_parallel(one, delete_ids, Path(result_file),  max_workers=4)
+    run_parallel(one, delete_ids, Path(result_file), max_workers=4)
